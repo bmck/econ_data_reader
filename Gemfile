9415996df7f0eda9172d8cf2f68e31fc936c42dc
Bill McKinnon <bill.mckinnon@bmck.org>
2024-06-06 14:13:57 -0500

    updates

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source "https://rubygems.org"
-
-# Specify your gem's dependencies in econ_data_reader.gemspec
-gemspec
-
-gem "rake", "~> 12.0"
-#gem "none", "~> "
--- lib/econ_data_reader.rb
+++ lib/econ_data_reader.rb
@@ -1,6 +1,34 @@
+# require 'rubygems'
+# gem 'httparty'
+# require 'httparty'
+
 require "econ_data_reader/version"
+require 'econ_data_reader/bank_of_canada'
+require 'econ_data_reader/bls'
+require 'econ_data_reader/fred'
+require 'econ_data_reader/nasdaq'
+
+directory = File.expand_path(File.dirname(__FILE__))
+
+
+# create config/initializers/econ_data_reader.rb
+#
+# EconDataReader.configure do |config|
+#   config.fred_api_key = 'api_key'
+# end
+# client = EconDataReader::Client.new
+#
+# or
+#
+# EconDataReader.fred_api_key = 'api_key'
+#
+# or
+#
+# EconDataReader::Fred.new(:fred_api_key => 'api_key')
+
 
 module EconDataReader
   class Error < StandardError; end
   # Your code goes here...
+  
 end
